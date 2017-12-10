e7d8972b101ebb244a7391405ee9c2b578da3128
Juanse Caro <juanseb@gmail.com>
2017-12-10 23:39:47 +0100

    minor fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,12 @@ tel: "644 25 20 97",
 web: "http://www.valledelosmolinos.com",
 mlat: 38.237891,
 mlon: -6.015007,
-schedule: "10:00 a 14:00 y de 16:00 a 17:00"
+schedule: "10:00 a 14:00 y de 16:00 a 17:00",
+description: "Un año más Valle de los Molinos saca su oferta de campamentos de verano para 2016. Campamentos de verano en Extremadura, donde disfrutamos de actividades multiaventura, naturaleza e inglés.
+Situado en un enclave idílico a los pies de Sierra Morena, podemos encontrar el valle de los molinos en las cercanías de Llerena, donde el joven podrá disfrutar de una experiencia inolvidable en  su campamentos de 15 días en Los Molinos realizando diferentes actividades en el medio natural, así como diferentes prácticas deportivas, visitas culturales y talleres de artesanía ofrecidos por expertos artesanos, además algunos de estos talleres son impartidos en la modalidad bilingüe, en inglés.
+En el Valle de los molinos, sabemos de la importancia del inglés como segunda lengua hoy en día, por eso la hemos incluido en nuestras actividades de forma natural. No queremos enfocarlo a difíciles y pesadas actividades escritas, sino a vocabulario natural hablado y enfocado a mejorar la fluidez y la respuesta a situaciones habituales, convirtiendo el inglés no en una asignatura, sino en una herramienta de comunicación que nos mejora la vida día tras día.",
+email: "juanmi@valledelosmolinos.com",
+address: "Molino de Viento, 14"
 
 
 
