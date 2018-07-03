6a0c2b93f5f2630f98aef3160f55bb0cf8ef6822
Juanse Caro <juanseb@gmail.com>
2018-07-03 13:12:59 +0200

    Superadmin achievement edits done

--- app/controllers/superadmin/users_controller.rb
+++ app/controllers/superadmin/users_controller.rb
@@ -5,11 +5,11 @@ class Superadmin::UsersController < Superadmin::ApplicationController
   end
 
   def show
-
+    @proposals = AchievementProposal.all
   end
 
   def edit
-
+    @proposals = AchievementProposal.all
   end
 
   def update
@@ -35,6 +35,6 @@ class Superadmin::UsersController < Superadmin::ApplicationController
     @user = User.find(params[:id])
   end
   def users_params
-    params.require(:user).permit(:email, :creditos, :empresa_id, :role, :first_name, :last_name, :birthdate, :dni, :phone, :address, :gender)
+    params.require(:user).permit(:email, :creditos, :empresa_id, :role, :first_name, :last_name, :birthdate, :dni, :phone, :address, :gender, achievement_attributes: [:achievement1, :achievement2, :achievement3, :achievement4, :achievement5, :achievement6, :achievement7, :achievement8, :achievement9, :achievement0, :_destroy])
   end
 end
