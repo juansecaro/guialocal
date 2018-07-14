108736ce75b4b32078d2a71961461cee74efc754
Juanse Caro <juanseb@gmail.com>
2018-07-14 21:16:13 +0200

    undo changes

--- config/initializers/instance_config.rb
+++ config/initializers/instance_config.rb
@@ -1,5 +1,5 @@
 # This places is reserved for city configs on initialization
 
-#$current_city = Config.try(:first).try(:city)
+$current_city = Config.try(:first).try(:city)
 
 $url_base = "http://www.adeter.org/guialocal"
