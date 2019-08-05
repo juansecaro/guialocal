e62756d27337b2ddc0613532e359b85af0813dca
Juanse Caro <juanseb@gmail.com>
2019-08-06 00:24:40 +0200

    adding public/packs

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,7 @@ append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+set :linked_dirs, fetch(:linked_dirs, []).push('public/packs', 'node_modules')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
