599e1589eea0a39f2195db5c10fb332a7aa3ff5c
Juanse Caro <juanseb@gmail.com>
2019-11-24 05:23:10 +0100

    Ambassadors titles fix 2

--- app/controllers/ambassadors_controller.rb
+++ app/controllers/ambassadors_controller.rb
@@ -2,32 +2,6 @@ class AmbassadorsController < ApplicationController
 
   def index
     @ambassadors = Ambassador.all
-
-    titulo ||= "Embajadores internacionales de Llerena, ESPAÑA"
-    site ||= "Guia#{ENV['CURRENT_CITY_CAP']}.es"
-    info = "Borderless Ambassadors"
-
-    set_meta_tags title: titulo,
-                site: site,
-                reverse: true,
-                description: info,
-                keywords: titulo,
-
-                twitter: {
-                  card: "summary",
-                  site: site,
-                  title: titulo + " | " + site,
-                  description:  info,
-                  image: "#{$url_base}/cities/#{ENV['CURRENT_CITY']}/europe_ambassadors.jpg"
-                },
-
-                og: {
-                  title:    titulo + " | " + site,
-                  description: info,
-                  type:     'article',
-                  url:      "ambassador_path(@ambassador)",
-                  image:    "#{$url_base}/cities/#{ENV['CURRENT_CITY']}/europe_ambassadors.jpg"
-                }
   end
 
   def list # index in english
@@ -97,73 +71,71 @@ class AmbassadorsController < ApplicationController
   end
 
   def english
-      @ambassador = Ambassador.friendly.find(params[:id])
+    @ambassador = Ambassador.find_by_slug(params[:name])
 
-      titulo ||= @ambassador.name + " | International Ambassador for Llerena (SPAIN)"
-      site ||= "Guia#{ENV['CURRENT_CITY_CAP']}.es"
-      info = "Embajadores sin fronteras"
+    titulo ||= @ambassador.name + " | International Ambassador for Llerena (SPAIN)"
+    site ||= "Guia#{ENV['CURRENT_CITY_CAP']}.es"
+    info = "Embajadores sin fronteras"
 
-      set_meta_tags title: titulo,
+    set_meta_tags title: titulo,
+                site: site,
+                reverse: true,
+                description: info,
+                keywords: titulo,
+
+                twitter: {
+                  card: "summary",
                   site: site,
-                  reverse: true,
+                  title: titulo + " | " + site,
+                  description:  info,
+                  image: @ambassador.picture.url
+                },
+
+                og: {
+                  title:    titulo + " | " + site,
                   description: info,
-                  keywords: titulo,
+                  type:     'article',
+                  url:      "ambassador_path(@ambassador)",
+                  image:    @ambassador.picture.url
+                }
 
-                  twitter: {
-                    card: "summary",
-                    site: site,
-                    title: titulo + " | " + site,
-                    description:  info,
-                    image: @ambassador.picture.url
-                  },
-
-                  og: {
-                    title:    titulo + " | " + site,
-                    description: info,
-                    type:     'article',
-                    url:      "ambassador_path(@ambassador)",
-                    image:    @ambassador.picture.url
-                  }
-
-      #Si no lo encuentra
-    rescue ActiveRecord::RecordNotFound
-      flash[:alert] = "La embajador que buscas no existe"
-      redirect_to (request.referrer || ambassadors_path)
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The ambassador you are looking for is not here, or not here anymore"
+    redirect_to (request.referrer || ambassadors_path)
   end
 
   def native
-      @ambassador = Ambassador.friendly.find(params[:id])
+    @ambassador = Ambassador.find_by_slug(params[:name])
 
-      titulo ||= @ambassador.name + " | International Ambassador for Llerena (SPAIN)"
-      site ||= "Guia#{ENV['CURRENT_CITY_CAP']}.es"
-      info = "Embajadores sin fronteras"
+    titulo ||= @ambassador.name + " | International Ambassador for Llerena (SPAIN)"
+    site ||= "Guia#{ENV['CURRENT_CITY_CAP']}.es"
+    info = "Embajadores sin fronteras"
 
-      set_meta_tags title: titulo,
+    set_meta_tags title: titulo,
+                site: site,
+                reverse: true,
+                description: info,
+                keywords: titulo,
+
+                twitter: {
+                  card: "summary",
                   site: site,
-                  reverse: true,
+                  title: titulo + " | " + site,
+                  description:  info,
+                  image: @ambassador.picture.url
+                },
+
+                og: {
+                  title:    titulo + " | " + site,
                   description: info,
-                  keywords: titulo,
+                  type:     'article',
+                  url:      "ambassador_path(@ambassador)",
+                  image:    @ambassador.picture.url
+                }
 
-                  twitter: {
-                    card: "summary",
-                    site: site,
-                    title: titulo + " | " + site,
-                    description:  info,
-                    image: @ambassador.picture.url
-                  },
-
-                  og: {
-                    title:    titulo + " | " + site,
-                    description: info,
-                    type:     'article',
-                    url:      "ambassador_path(@ambassador)",
-                    image:    @ambassador.picture.url
-                  }
-
-      #Si no lo encuentra
-    rescue ActiveRecord::RecordNotFound
-      flash[:alert] = "La embajador que buscas no existe"
-      redirect_to (request.referrer || ambassadors_path)
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The ambassador you are looking for is not here, or not here anymore"
+    redirect_to (request.referrer || ambassadors_path)
   end
 
 end
