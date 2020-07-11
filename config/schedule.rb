349ec9afb09fef347c2303e7f363b6ee92ecac70
Juanse Caro <juanseb@gmail.com>
2020-07-11 12:31:27 +0200

    Cron adjustments to rbenv

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,15 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
+set :env_path,    '"$HOME/.rbenv/shims":"$HOME/.rbenv/bin"'
+
+# doesn't need modifications
+# job_type :command, ":task :output"
+
+job_type :rake,   %q{ cd :path && PATH=:env_path:"$PATH" RAILS_ENV=:environment bin/rake :task --silent :output }
+job_type :runner, %q{ cd :path && PATH=:env_path:"$PATH" bin/rails runner -e :environment ':task' :output }
+job_type :script, %q{ cd :path && PATH=:env_path:"$PATH" RAILS_ENV=:environment bundle exec bin/:task :output }
+
 # Example:
 #
 # set :output, "/path/to/my/cron_log.log"
